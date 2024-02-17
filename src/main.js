69a3e1801552b15754cb9ef58dbe6d6210274554
benhodges65 <77591479+benhodges65@users.noreply.github.com>
2024-02-17 14:29:33 -0600

    first commit

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    {
+      name: 'teams',
+      path: '/teams',
+      components: {default: TeamsList, footer: TeamsFooter},
+      children: [
+        { name: 'team-members', path: ':teamId', component: TeamMembers },
+      ],
+    },
+    { path: '/users', component: {default: UsersList, footer: UsersFooter}},
+    { path: '/:notFound(.*)', redirect: '/teams' },
+  ],
+  linkActiveClass: 'active',
+  scrollBehavior (to, from, savedPosition) {
+    console.log(to, from, savedPosition);
+    if(savedPosition){
+        return savedPosition;
+    }
+  }
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
